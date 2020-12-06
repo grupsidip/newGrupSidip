aa6108ba546c18c8b905db97b720c80be31c7c7a
JC. Beneyto <jnk.beneyto@gmail.com>
2020-12-06 18:05:53 +0100

    adding titles on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,26 @@ import '../../src/styles/style.scss'
 
 const IndexPage = () => (
   <Layout className="d-flex align-self-center" pageInfo={{ pageName: "index" }}>
-    <SEO title="Home" keywords={[`peritos`, `documentoscopia`, `poligrafo`]} />
-    <Container className="text-center index rounded " fluid>
+    <SEO title="Home" keywords={[`peritos`, `documentoscopia`, `poligrafo`, `detectives Brasil`, `detective Brazil`]} />
+    <Container className="indexPage" >
+      <Row className="d-flex justify-content-center align-self-center mt-0" >
+        <Col >
+          <p className="text text-center maintitle"> <strong> DETECTIVES BRASIL</strong></p>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center align-self-center" >
+        <Col >
+          <p className="text text-center title">INVESTIGACIONES PARTICULARES Y CORPORATIVAS</p>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center align-self-center" >
+        <Col >
+          <p className="text text-center title">CRIMINAL COMPLIANCE </p>
+        </Col>
+      </Row>
+
       <Row className="">
-        <Col className="">
+        <Col className="text-center index rounded " fluid>
 
         </Col>
       </Row>
